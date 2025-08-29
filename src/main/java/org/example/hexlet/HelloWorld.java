3f8b8ad6357c6abd0d22cb777199666c804d7b64
Lakever <givster@yandex.ru>
2025-08-29 21:00:54 +0300

    somethink

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,22 +1,32 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursesPage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
         // Описываем, что загрузится по адресу /
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course("English","Learning English"));
+        courses.add(new Course("German","Learning German"));
+        app.get("/courses", ctx -> {
 
-        // GET /hello?name=John
-        app.get("/hello", ctx -> {
-            var name = ctx.queryParam("name");
-            if (name == null || name.isBlank()) {
-                name = "World";
-            }
-            ctx.result("Hello, " + name + "!");
+            var header = "Языковые курсы";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/show.jte", model("page", page));
         });
         app.start(7070); // Стартуем веб-сервер
         //da
